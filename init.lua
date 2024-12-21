44f57ae296eced8ebf9b1821777a58c4cee25676
malemare <malemare@2a02-2454-85d9-1d00-dc21-4601-3514-866c.dyn6.pyur.net>
2024-12-20 19:21:53 -0500

    add init options from vimrc

--- init.lua
+++ init.lua
@@ -1,6 +1,37 @@
-require("malmar")
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.g.mapleader = " "
 
-vim.cmd [[cd d:\\tasks]]
+vim.keymap.set("n", "<leader>er", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>ew", "<cmd>:tabnew<cr>")
+vim.keymap.set("n", "<leader>eq", "<cmd>:q<cr>")
+
+vim.opt.number = true
+vim.opt.visualbell = true
+
+vim.opt.wrap =true
+vim.opt.textwidth =79
+vim.opt.tabstop =4
+vim.opt.shiftwidth =4
+vim.opt.softtabstop =4
+vim.opt.expandtab =true
+
+vim.opt.scrolloff =3
+vim.opt.backspace =indent,eol,start
+
+vim.opt.laststatus =2
+vim.opt.showmode =true
+vim.opt.showcmd =true
+
+vim.opt.hlsearch =true
+vim.opt.incsearch =true
+vim.opt.ignorecase =true
+vim.opt.smartcase =true
+vim.opt.showmatch =true
+
+vim.opt.cursorline =true
+
+vim.cmd [[colorscheme darkblue]]
+
+vim.cmd [[cd ~/tasks]]
+
+vim.cmd [[autocmd InsertEnter * set bg]]
