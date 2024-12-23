d4925da797759fcb65b6f07b25b6e204b817efe9
Malek Malefix <voiceofshade@gmail.com>
2024-12-23 12:59:32 -0800

    use functions in keymap.set

--- init.lua
+++ init.lua
@@ -1,37 +1,59 @@
 
-vim.g.mapleader = " "
+vim.opt.autochdir =true
 
-vim.keymap.set("n", "<leader>er", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>ew", "<cmd>:tabnew<cr>")
-vim.keymap.set("n", "<leader>eq", "<cmd>:q<cr>")
+vim.opt.visualbell =true
 
-vim.opt.number = true
-vim.opt.visualbell = true
+vim.opt.cursorcolumn =false
+vim.opt.cursorline =true
+
+vim.opt.ignorecase =true
+vim.opt.incsearch =true
+vim.opt.smartcase =true
+vim.opt.hlsearch =true
+vim.opt.showmatch =true
+
+vim.opt.shiftwidth =4
+vim.opt.expandtab =true
+vim.opt.autoindent =true
+
+vim.opt.ruler =true
+vim.opt.number =true
 
 vim.opt.wrap =true
 vim.opt.textwidth =79
-vim.opt.tabstop =4
-vim.opt.shiftwidth =4
-vim.opt.softtabstop =4
-vim.opt.expandtab =true
 
-vim.opt.scrolloff =3
 vim.opt.backspace =indent,eol,start
 
 vim.opt.laststatus =2
 vim.opt.showmode =true
 vim.opt.showcmd =true
 
-vim.opt.hlsearch =true
-vim.opt.incsearch =true
-vim.opt.ignorecase =true
-vim.opt.smartcase =true
-vim.opt.showmatch =true
 
-vim.opt.cursorline =true
+vim.g.mapleader =" "
+vim.keymap.set("n", "<leader>er", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>nn", 
+    function()
+        vim.api.nvim_command('tabnew')
+    end,
+    {noremap =true})
 
-vim.cmd [[colorscheme darkblue]]
+vim.keymap.set("n", "<leader>eq", 
+    function()
+        vim.api.nvim_command('quit')
+    end,
+    {noremap =true})
+
+vim.keymap.set("n", "<leader>ew",
+    function()
+        vim.api.nvim_command('write')
+    end,
+    {noremap =true} 
+)
+
 
 vim.cmd [[cd ~/tasks]]
+vim.cmd [[colorscheme retrobox]]
+vim.cmd [[autocmd InsertEnter * set cursorcolumn]]
+vim.cmd [[autocmd InsertLeave * set nocursorcolumn]]
 
-vim.cmd [[autocmd InsertEnter * set bg]]
+
